499ef8e3ae1229c209a8e1ae707dcc997182dcea
MikePai <ye518612@gmail.com>
2020-11-01 21:01:47 +0800

    Initial Commit

---
+++ MoneyRecord/MoneyRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyRecordApp.swift
+//  MoneyRecord
+//
+//  Created by 白謹瑜 on 2020/11/1.
+//
+
+import SwiftUI
+
+@main
+struct MoneyRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
